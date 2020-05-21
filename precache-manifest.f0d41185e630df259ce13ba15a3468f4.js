2a74a1e6e417064aa890da5f2984449820956dde
matthewlicausi <matthewlicausi@gmail.com>
2020-05-21 08:05:05 -0500

    Updates

---
+++ precache-manifest.f0d41185e630df259ce13ba15a3468f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32dd768ceca8c898926e1c2f902d030",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3aaaa7efa2916d6275ce",
+    "url": "/react-monsters-rolodex/static/css/main.2e9e9a99.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/react-monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aaaa7efa2916d6275ce",
+    "url": "/react-monsters-rolodex/static/js/main.6c61fc11.chunk.js"
+  },
+  {
+    "revision": "5d1bcaa8ffa01b561da5",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.ab59da59.js"
+  }
+]);
